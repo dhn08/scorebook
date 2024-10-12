fbaa75f842cb2bf15b7f2b35c63b2c48c6c24265
Dhananjay Nathawat <singhdhananjay.2001@gmail.com>
2024-10-12 21:10:04 +0530

    allowed cors origin from frontend prod link

--- src/app.js
+++ src/app.js
@@ -2,12 +2,25 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  }),
-);
+
+const allowedOrigins = [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN_PROD];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   }),
+// );
 app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
